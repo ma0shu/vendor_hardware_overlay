65f13d71e94ccdfcaa8ba290b7b720385cb9ba57
Desmond <huixiaqingliu@outlook.com>
2025-10-28 21:21:11 +0800

    Add overlay for unknown brank device - NEWOS M70 Pro

--- overlay.mk
+++ overlay.mk
@@ -138,6 +138,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-moto-tundra \
 	treble-overlay-moto-tundra-systemui \
 	treble-overlay-mtk-ims \
+	treble-overlay-newos-m70pro \
 	treble-overlay-nokia-b2n-7plus \
 	treble-overlay-nokia-ctl-7-1 \
 	treble-overlay-nokia-drg-6.1plus-x6 \
